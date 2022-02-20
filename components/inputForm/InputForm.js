7c1de9c420c8e52c8cb9a8705defadd80aa7e47c
repro-tetris <77537560+repro-tetris@users.noreply.github.com>
2022-02-20 16:29:06 +0200

    input form styled

--- components/inputForm/InputForm.js
+++ components/inputForm/InputForm.js
@@ -1,6 +1,7 @@
 import fetch from "isomorphic-unfetch";
 import Transaction from "../../model/transaction";
 import Router from "next/router";
+import s from "./InputForm.module.css";
 
 export default function InputForm({ type, userId }) {
   const onSubmit = async (event) => {
@@ -22,19 +23,35 @@ export default function InputForm({ type, userId }) {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="description">Description</label>
-      <input id="description" type="text" name="description" required></input>
-      <label htmlFor="amount">Amount</label>
-      <input
-        id="amount"
-        type="number"
-        name="amount"
-        step="0.01"
-        min="0"
-        required
-      ></input>
-      <button type="submit">Add</button>
-    </form>
+    <div className={s.container}>
+      <h2>{type}</h2>
+      <form className={s.form} onSubmit={onSubmit}>
+        <label className={s.label} htmlFor="description">
+          Description
+        </label>
+        <input
+          className={s.input}
+          id="description"
+          type="text"
+          name="description"
+          required
+        ></input>
+        <label className={s.label} htmlFor="amount">
+          Amount
+        </label>
+        <input
+          className={s.input}
+          id="amount"
+          type="number"
+          name="amount"
+          step="0.01"
+          min="0"
+          required
+        ></input>
+        <button className={s.button} type="submit">
+          Add
+        </button>
+      </form>
+    </div>
   );
 }
