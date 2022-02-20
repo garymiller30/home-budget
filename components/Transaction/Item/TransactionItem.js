f4e96288576266fdabda3c1fdfc32f2f00468d8b
repro-tetris <77537560+repro-tetris@users.noreply.github.com>
2022-02-20 18:44:25 +0200

    table styles

--- components/Transaction/Item/TransactionItem.js
+++ components/Transaction/Item/TransactionItem.js
@@ -1,7 +1,11 @@
 export default function TransactionItem({ transaction }) {
   return (
-    <li>
-      {transaction.description}: {transaction.amount}₴
-    </li>
+    // <li>
+    //   {transaction.description}: {transaction.amount}₴
+    // </li>
+    <tr>
+      <td>{transaction.description}</td>
+      <td>{transaction.amount}</td>
+    </tr>
   );
 }
--- components/Transaction/List/TransactionList.js
+++ components/Transaction/List/TransactionList.js
@@ -2,14 +2,30 @@ import TransactionItem from "../Item/TransactionItem";
 import s from "./TransactionList.module.css";
 
 export default function TransactionList({ transactions }) {
+  // return (
+  //   <ul className={s.list}>
+  //     {transactions.map((transaction) => (
+  //       <TransactionItem
+  //         key={transaction._id}
+  //         transaction={transaction}
+  //       ></TransactionItem>
+  //     ))}
+  //   </ul>
+  // );
+
   return (
-    <ul className={s.list}>
-      {transactions.map((transaction) => (
-        <TransactionItem
-          key={transaction._id}
-          transaction={transaction}
-        ></TransactionItem>
-      ))}
-    </ul>
+    <table className={s.table}>
+      <thead>
+        <tr>
+          <th>description</th>
+          <th>amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map((transaction) => (
+          <TransactionItem key={transaction._id} transaction={transaction} />
+        ))}
+      </tbody>
+    </table>
   );
 }
