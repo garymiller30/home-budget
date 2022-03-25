38b41cd1eca404b6a82c51dca309a128052d13ec
repro-tetris <garymiller30@gmail.com>
2022-03-25 21:05:46 +0200

    Update TransactionGroup.js

--- components/Transaction/Group/TransactionGroup.js
+++ components/Transaction/Group/TransactionGroup.js
@@ -2,12 +2,12 @@ import s from "./TransactionGroup.module.css";
 export default function TransactionGroup({ title, children }) {
   return (
     <>
-      {/* <tr className={s.tr}>
+      <tr className={s.tr}>
         <td colSpan="2" className={s.title}>
           {title}
         </td>
         <td></td>
-      </tr> */}
+      </tr>
       {children}
     </>
   );
