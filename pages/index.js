2eb877d283d325cbf55d95b6d3501112871d5fa2
repro-tetris <garymiller30@gmail.com>
2022-04-01 14:25:16 +0300

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import { useSession } from "next-auth/react";
 import { getSession } from "next-auth/react";
 import { getUser } from "../db/user";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 import Image from "next/image";
+import Button from "@mui/material/Button";
 import s from "./index.module.css";
 
 import { Circles } from "react-loader-spinner";
@@ -19,18 +22,20 @@ export default function Home() {
     );
   } else {
     return (
-      <div className={s.container}>
-        <button className={s.button} onClick={() => Router.push("/login")}>
-          <div className={s.img}>
-            <Image
-              src="/google-logo.svg"
-              width={32}
-              height={32}
-              alt="google logo"
-            />
-          </div>
-          Sign in with Google
-        </button>
+      <div className={`${s.container} ${s.bg}`}>
+        <Stack spacing={5}>
+          <Typography variant="h2" className={s.title}>
+            simple budget
+            <br /> app
+          </Typography>
+          <Button
+            onClick={() => Router.push("/login")}
+            variant="contained"
+            size="large"
+          >
+            Sign in with Google
+          </Button>
+        </Stack>
       </div>
     );
   }
