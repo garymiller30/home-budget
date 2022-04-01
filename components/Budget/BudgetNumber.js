3853439bcdc3dd6ebf5ec02ddb25ce92c138c1dc
repro-tetris <garymiller30@gmail.com>
2022-04-01 15:02:31 +0300

    Update BudgetNumber.js

--- components/Budget/BudgetNumber.js
+++ components/Budget/BudgetNumber.js
@@ -4,7 +4,7 @@ export default function BudgetNumber({ budget }) {
   const budgetMain = Math.trunc(budget);
   const budgetKop = Math.trunc((budget % 1).toFixed(2) * 100);
   return (
-    <Typography component={"span"} className={s.budgetMain}>
+    <Typography component={"div"} className={s.budgetMain}>
       {budgetMain}
       <span className={s.budgetKop}>{budgetKop}</span>₴
     </Typography>
