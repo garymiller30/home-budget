c0ebf46057016ffe253ee6206f886371d8a26461
repro-tetris <garymiller30@gmail.com>
2022-04-01 09:58:11 +0300

    Update BudgetNumber.js

--- components/Budget/BudgetNumber.js
+++ components/Budget/BudgetNumber.js
@@ -1,7 +1,7 @@
 import s from "./BudgetNumber.module.css";
 export default function BudgetNumber({ budget }) {
   const budgetMain = Math.trunc(budget);
-  const budgetKop = (budget % 1).toFixed(2) * 100;
+  const budgetKop = Math.trunc((budget % 1).toFixed(2) * 100);
   return (
     <p className={s.budgetMain}>
       {budgetMain}
