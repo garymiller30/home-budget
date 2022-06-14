bb954f445086d4eec7f156cda0eb11bee9686e5c
garymiller30 <garymiller30@gmail.com>
2022-06-14 11:27:34 +0300

    Update convertDateToIso.ts

--- utils/convertDateToIso.ts
+++ utils/convertDateToIso.ts
@@ -1,6 +1,8 @@
 export function convertDateToIso(localDate: string): Date {
 
     const s = localDate.split(/\D/);
-    return new Date(
-        `${s[2]}-${s[1]}-${s[0]}T${s[4]}:${s[5]}:${s[6]}`)
+    const [day, month, year, empty, hour, min, sec] = s;
+    return new Date(+year, Number(month) - 1, +day, +hour, +min, +sec)
+    // return new Date(
+    //     `${s[2]}-${s[1]}-${s[0]}T${s[4]}:${s[5]}:${s[6]}`)
 }
