d6dbfcf9414f31c311b05d29e232f593be8a8e3c
garymiller30 <garymiller30@gmail.com>
2022-06-19 11:20:08 +0300

    Update DateNavigator.tsx

--- components/DateNavigator/DateNavigator.tsx
+++ components/DateNavigator/DateNavigator.tsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import MobileDatePicker from "@mui/lab/DatePicker";
+//import MobileDatePicker from "@mui/lab/DatePicker";
 import Box from "@mui/material/Box";
 import { iDate } from "../../interfaces/iDate";
+import dynamic from "next/dynamic";
+
+const DynamicMobileDatePicker = dynamic(() => import("@mui/lab/DatePicker"));
 
 interface DateNavigatorProps {
   date: iDate;
@@ -19,19 +22,20 @@ export default function DateNavigator({
     new Date(date.year, date.month - 1)
   );
 
-  function handleOnChange(d: Date) {
-    onChangeDate({ year: d.getFullYear(), month: d.getMonth() + 1 });
+  function handleOnChange(d: Date | unknown) {
+    if (d instanceof Date)
+      onChangeDate({ year: d.getFullYear(), month: d.getMonth() + 1 });
   }
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <Box sx={{ width: 130 }}>
-        <MobileDatePicker
+        <DynamicMobileDatePicker
           inputFormat="yyyy-MM"
           views={["year", "month"]}
           maxDate={new Date()}
           value={value}
-          onChange={(e) => setValue(e)}
+          onChange={(e: any) => setValue(e)}
           onMonthChange={handleOnChange}
           renderInput={(params) => (
             <TextField {...params} helperText={null} size="small" />
