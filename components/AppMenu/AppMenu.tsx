a285bd51eeee613a94d5e92b9ed8213537dad9e9
garymiller30 <garymiller30@gmail.com>
2022-07-19 17:23:27 +0300

    Update AppMenu.tsx

--- components/AppMenu/AppMenu.tsx
+++ components/AppMenu/AppMenu.tsx
@@ -1,5 +1,7 @@
 import { Menu, MenuItem } from "@mui/material";
 import { useState } from "react";
+import ModalInputForm from "../ModalInputForm/ModalInputForm";
+import PriceOfUnitComponent from "../PriceOfUnitComponent/PriceOfUnitComponent";
 
 type AppMenuProp = {
   open: boolean;
@@ -8,9 +10,21 @@ type AppMenuProp = {
 };
 
 export default function AppMenu({ open, close, anchor }: AppMenuProp) {
+  const [openModal, setOpenModal] = useState<boolean>(false);
   return (
-    <Menu open={open} anchorEl={anchor} onClose={close}>
-      <MenuItem>Price of unit</MenuItem>
-    </Menu>
+    <>
+      <Menu open={open} anchorEl={anchor} onClose={close}>
+        <MenuItem onClick={() => setOpenModal(true)}>Price of unit</MenuItem>
+      </Menu>
+      <ModalInputForm
+        show={openModal}
+        onClose={() => {
+          setOpenModal(false);
+        }}
+        title="Price of unit"
+      >
+        <PriceOfUnitComponent />
+      </ModalInputForm>
+    </>
   );
 }
