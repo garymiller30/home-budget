0082df87cdb1465fc0e434d0285b4564eac3111b
garymiller30 <garymiller30@gmail.com>
2022-07-19 23:45:51 +0300

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,10 @@
 import { Box } from "@mui/material";
 import { signIn } from "next-auth/react";
+import { useEffect } from "react";
 export default function login() {
-  signIn("google", { callbackUrl: `${process.env.ROOT_URL}` });
+  useEffect(() => {
+    signIn("google", { callbackUrl: "/" });
+  }, []);
+
   return <Box>Login page</Box>;
 }
