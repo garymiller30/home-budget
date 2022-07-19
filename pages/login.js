30352e6aa2d3a009f5e954af56cab3a592988bd8
garymiller30 <garymiller30@gmail.com>
2022-07-19 23:49:10 +0300

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,7 @@
-import { Box } from "@mui/material";
 import { signIn } from "next-auth/react";
-import { useEffect } from "react";
-export default function login() {
-  useEffect(() => {
-    signIn("google", { callbackUrl: "/" });
-  }, []);
 
-  return <Box>Login page</Box>;
+export default function login() {
+  signIn("google", { callbackUrl: "/" });
+
+  return <></>;
 }
