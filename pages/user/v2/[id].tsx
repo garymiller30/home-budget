bf73ea1bbe234044c3da9cdd9028ab9239cc586c
garymiller30 <garymiller30@gmail.com>
2022-07-20 13:58:51 +0300

    Update [id].tsx

--- pages/user/v2/[id].tsx
+++ pages/user/v2/[id].tsx
@@ -66,8 +66,12 @@ export default function User({ user }: UserProps) {
             display: "flex",
             flexDirection: "column",
             height: "100vh",
-            maxHeight: "100%",
             width: "100%",
+            position: "fixed",
+            left: "0",
+            top: "0",
+            right: "0",
+            bottom: "0",
           }}
         >
           <UserAppBar />
