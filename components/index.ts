aeee28ddad93d2545899b8a2afba101e0fc52143
garymiller30 <garymiller30@gmail.com>
2022-07-24 20:48:33 +0300

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,25 +1,19 @@
-import AddCreditBtn from "./AddCreditBtn/AddCreditBtn";
-import AddDebitBtn from "./AddDebitBtn/AddDebitBtn";
 import InputForm from "./Forms/InputForm";
 
-import Budget from "./Budget/Budget";
+
 import ModalInputForm from "./ModalInputForm/ModalInputForm";
-import DateNavigator from "./DateNavigator/DateNavigator";
-import BottomNavigation from "./BottomNavigation/BottomNavigation";
+
+
 import ChartBtn from "./ChartBtn/ChartBtn";
-import UserMenu from "./UserMenu/UserMenu";
-import TransactionContainer from "./Transaction/Container/TransactionContainer";
-import TransactionTable from "./Transaction/Table/TransactionTable";
+
+
+
 export {
-  AddCreditBtn,
-  AddDebitBtn,
+
   InputForm,
-  Budget,
+
   ModalInputForm,
-  DateNavigator,
-  BottomNavigation,
+
   ChartBtn,
-  UserMenu,
-  TransactionContainer,
-  TransactionTable,
+
 };
