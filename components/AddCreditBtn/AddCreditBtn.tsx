4c07f2eb07b07cf97711859e82055860a79d20a3
garymiller30 <garymiller30@gmail.com>
2022-07-24 13:13:12 +0300

    fix

--- components/AddCreditBtn/AddCreditBtn.tsx
+++ components/AddCreditBtn/AddCreditBtn.tsx
@@ -1,13 +1,15 @@
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 
 interface AddCreditBtnProps {
   onClick: () => void;
 }
 
 export default function AddCreditBtn({ onClick }: AddCreditBtnProps) {
-  return (
-    <Button variant="contained" color="secondary" onClick={onClick}>
-      add credit
-    </Button>
-  );
+  return null;
+  /* A JSX expression. */
+  //   (
+  //   <Button variant="contained" color="secondary" onClick={onClick}>
+  //     add credit
+  //   </Button>
+  // );
 }
--- components/AddDebitBtn/AddDebitBtn.tsx
+++ components/AddDebitBtn/AddDebitBtn.tsx
@@ -1,19 +1,20 @@
 import s from "./AddDebitBtn.module.css";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 
 interface AddDebitBtnProps {
   onClick: () => void;
 }
 
 export default function AddDebitBtn({ onClick }: AddDebitBtnProps) {
-  return (
-    <Button
-      variant="contained"
-      color="primary"
-      className={s.btn}
-      onClick={onClick}
-    >
-      add debit
-    </Button>
-  );
+  return null;
+  //   (
+  //   <Button
+  //     variant="contained"
+  //     color="primary"
+  //     className={s.btn}
+  //     onClick={onClick}
+  //   >
+  //     add debit
+  //   </Button>
+  // );
 }
