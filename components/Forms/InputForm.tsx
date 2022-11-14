f35c7ce0f0400893d7980806e400add93f35c7ef
garymiller30 <garymiller30@gmail.com>
2022-11-14 13:50:50 +0200

    Update InputForm.tsx

--- components/Forms/InputForm.tsx
+++ components/Forms/InputForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import Transaction from "../../model/transaction";
 import { TRANSACTION_TYPE } from "../../vars/variables";
 import { useTransactionController } from "../../hooks/useTransactionController";
@@ -6,13 +6,16 @@ import {
   Button,
   CircularProgress,
   Container,
+  Flex,
   FormControl,
   FormLabel,
+  IconButton,
   Input,
   Spacer,
   Stack,
   Text,
 } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 interface InputFormProps {
   type: TRANSACTION_TYPE;
@@ -23,6 +26,9 @@ interface InputFormProps {
 export default function InputForm({ type, userId, onClose }: InputFormProps) {
   const contoller = useTransactionController();
   const [saving, setSaving] = useState<boolean>(false);
+
+  const commentRef = useRef<HTMLInputElement>(null);
+  const amountRef = useRef<HTMLInputElement>(null);
 
   const onSubmit = async (event: any) => {
     event.preventDefault();
@@ -50,17 +56,37 @@ export default function InputForm({ type, userId, onClose }: InputFormProps) {
     <Container sx={{ padding: "6px 0" }}>
       <form onSubmit={onSubmit}>
         <Stack spacing={2}>
-          <FormControl isRequired>
-            <FormLabel>Description</FormLabel>
-            <Input placeholder="Description" id="description" autoFocus />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Comment</FormLabel>
-            <Input placeholder="Comment" id="comment" />
-          </FormControl>
+          <Flex alignItems="end">
+            <FormControl isRequired>
+              <FormLabel>Description</FormLabel>
+              <Input placeholder="Description" id="description" autoFocus />
+            </FormControl>
+            <IconButton
+              aria-label="next"
+              icon={<ArrowForwardIcon />}
+              onClick={() => commentRef.current?.focus()}
+            />
+          </Flex>
+          <Flex alignItems="end">
+            <FormControl>
+              <FormLabel>Comment</FormLabel>
+              <Input ref={commentRef} placeholder="Comment" id="comment" />
+            </FormControl>
+            <IconButton
+              aria-label="next"
+              icon={<ArrowForwardIcon />}
+              onClick={() => amountRef.current?.focus()}
+            />
+          </Flex>
           <FormControl isRequired>
             <FormLabel>Amount</FormLabel>
-            <Input type="number" step="0.01" id="amount" placeholder="Amount" />
+            <Input
+              ref={amountRef}
+              type="number"
+              step="0.01"
+              id="amount"
+              placeholder="Amount"
+            />
           </FormControl>
 
           <Spacer />
