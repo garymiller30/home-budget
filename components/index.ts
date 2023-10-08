77797e571dadceb8ad1dfd2bd1b1f70348fdaff8
garymiller30 <garymiller30@gmail.com>
2023-10-08 11:56:32 +0300

    fix exports

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,8 @@
 import InputForm from "./Forms/InputForm";
 import ModalInputForm from "./ModalInputForm/ModalInputForm";
-import ChartBtn from "./ChartBtn/ChartBtn";
+
 
 export {
   InputForm,
   ModalInputForm,
-  ChartBtn,
 };
