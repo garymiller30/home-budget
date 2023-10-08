5f204bb8a40cc577f48030b2fea3a85deae29203
garymiller30 <garymiller30@gmail.com>
2023-10-08 12:33:15 +0300

    add day balance

--- components/UserLastTransactions/UserLastTransactionList.tsx
+++ components/UserLastTransactions/UserLastTransactionList.tsx
@@ -7,8 +7,18 @@ import {
   AccordionPanel,
   Box,
   List,
+  Stack,
+  Text,
 } from "@chakra-ui/react";
+import { PieController } from "chart.js";
+import { TRANSACTION_TYPE } from "vars/variables";
 import UserLastTransactionsItem from "./UserLastTransactionsItem";
+
+interface iDayResult {
+  credit: number;
+  debit: number;
+  budget: number;
+}
 
 export default function UserLastTransactionList({
   transactions,
@@ -27,13 +37,36 @@ export default function UserLastTransactionList({
     return p;
   }, {});
 
+  function getDayBudget(tl: iTransaction[]): iDayResult {
+    const result: { credit: number; debit: number; budget: number } = tl.reduce(
+      (p: iDayResult, c: iTransaction) => {
+        const amount = Number(c.amount);
+        if (c.type === TRANSACTION_TYPE.CREDIT) {
+          p.credit += amount;
+          p.budget -= amount;
+        } else {
+          p.debit += amount;
+          p.budget += amount;
+        }
+
+        return p;
+      },
+      { credit: 0, debit: 0, budget: 0 }
+    );
+
+    return result;
+  }
+
   return (
-    <Accordion defaultIndex={[0]} allowMultiple allowToggle>
+    <Accordion defaultIndex={[0]} allowMultiple>
       {Object.keys(groupByDate).map((x: any) => (
         <AccordionItem key={x}>
           <AccordionButton>
             <Box as="span" flex="1" textAlign="left">
-              {x}
+              <AccordionTitle
+                title={x}
+                dayResult={getDayBudget(groupByDate[x])}
+              />
             </Box>
             <AccordionIcon />
           </AccordionButton>
@@ -51,5 +84,37 @@ export default function UserLastTransactionList({
         </AccordionItem>
       ))}
     </Accordion>
+  );
+}
+
+function AccordionTitle({
+  title,
+  dayResult,
+}: {
+  title: string;
+  dayResult: iDayResult;
+}) {
+  return (
+    <Stack>
+      <Text fontWeight={"bold"} fontSize={14}>
+        {title} [
+        <Text
+          fontWeight={"semibold"}
+          as="span"
+          color={"green.600"}
+        >{`+${dayResult.debit},`}</Text>
+        <Text
+          fontWeight={"semibold"}
+          as="span"
+          color={"pink.500"}
+        >{` -${dayResult.credit},`}</Text>
+        <Text
+          fontWeight={"semibold"}
+          as="span"
+          color={"black.500"}
+        >{` ${dayResult.budget}`}</Text>
+        ]
+      </Text>
+    </Stack>
   );
 }
