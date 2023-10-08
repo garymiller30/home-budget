bbc6c978530b2337f7afd973a5ae49c794f24a2c
garymiller30 <garymiller30@gmail.com>
2023-10-08 11:54:51 +0300

    remove unused component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,14 @@
 import { useSession, getSession } from "next-auth/react";
 import { getUser } from "../db/user";
-// import Typography from "@mui/material/Typography";
-// import Stack from "@mui/material/Stack";
-// import Button from "@mui/material/Button";
 import s from "./index.module.css";
 import { Circles } from "react-loader-spinner";
-//import Router, { useRouter } from "next/router";
-import { iUser } from "../interfaces/iUser";
 import { GetServerSideProps } from "next";
-//import { useEffect } from "react";
 import { signIn } from "next-auth/react";
-import { Box, Button, HStack, Text, VStack } from "@chakra-ui/react";
-import PaperCutEffect from "@/components/PaperCutEffect/PaperCutEffect";
+import { Box, Button, VStack } from "@chakra-ui/react";
 
-interface HomeProps {
-  user: iUser;
-}
 export default function Home() {
   const { status } = useSession();
-  // const router = useRouter();
 
-  // useEffect(() => {
-  //   if (user) {
-  //     router.push(`/user/v2/${user._id}`);
-  //   }
-  // }, []);
-
-  //console.log("user:", user);
   if (status === "loading" || status === "authenticated") {
     return (
       <div className={s.spinner}>
@@ -48,10 +30,6 @@ export default function Home() {
           alignItems="center"
           justifyContent="center"
         >
-          <Box border="white 4px dashed" p={5} borderRadius={15}>
-            <PaperCutEffect text="HOME" />
-            <PaperCutEffect text="BUDGET" />
-          </Box>
           <Button
             onClick={() => {
               signIn("google", { callbackUrl: "/" });
